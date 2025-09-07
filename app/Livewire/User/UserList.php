c2b8a02f1bc1fd31d88d6150231ee115b04f628e
IvanTsumbek <maincumbek@gmail.com>
2025-09-07 20:59:50 +0300

    Урок 4. Валидация

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -4,25 +4,53 @@ namespace App\Livewire\User;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class UserList extends Component
 {
+    #[Validate('required')]
+    #[Validate('min:2', as:'Имя')]
+    #[Validate('max:30', message:'Слишком длинное имя')]
     public string $name;
+    #[Validate('required|email|max:30')]
     public string $email;
+    #[Validate('required|min:6')]
     public string $password;
 
-    public function addUser()
+    protected function rules(): array
     {
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => $this->password,
-        ]);
-
-        $this->reset('name', 'email', 'password');
+        return [
+                    'name' => 'required|min:2|max:30',
+                    'email' => 'required|email|max:30',
+                    'password' => 'required|min:6',
+                ];
     }
 
-    public function deleteUser(int $id)
+    protected function messages():array
+    {
+        return [
+            'name.required' => 'Name is required!!!',
+        ];
+    }
+
+    public function save()
+    {
+        // $this->all();                             собирает все данные в массив
+        // $this->only(['name', 'password']);        собирает выбранные
+        // $this->pull(['name', 'password']);        only + reset
+    //    $validated = $this->validate([
+    //         'name' => 'required|min:2|max:30',
+    //         'email' => 'required|email|max:30',
+    //         'password' => 'required|min:6',
+    //     ]);
+        $validated = $this->validate();
+        User::create($validated);
+
+        $this->reset();                                 //очистка всех свойств
+        // $this->reset('name', 'email', 'password');   очистка выбраных свойств
+    }
+
+    public function delete(int $id)
     {
         User::find($id)->delete();
     }
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -2,36 +2,41 @@
 
     <div class="col md-6">
 
-        <form wire:submit="addUser">
+        <form wire:submit="save">
             <div class="mb-3">
-                <input name="name" type="text" class="form-control" wire:model='name' placeholder="Name">
+                <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" wire:model.live='name' placeholder="Name">
+                @error('name')
+                    <div class="invalid-feedback"> {{ $message }} </div>
+                @enderror
             </div>
             <div class="mb-3">
-                <input type="email" class="form-control" wire:model='email' placeholder="Email">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" wire:model.blur='email' placeholder="Email">
+                @error('email')
+                <div class="invalid-feedback"> {{ $message }} </div>
+            @enderror
             </div>
             <div class="mb-3">
-                <input type="password" class="form-control" wire:model='password' placeholder="Password">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" wire:model.blur='password' placeholder="Password">
+                @error('password')
+                    <div class="invalid-feedback"> {{ $message }} </div>
+                @enderror
             </div>
             <div class="d-flex align-items-center gap-3">
                 <button type="submit" class="btn btn-primary my-2">Add user</button>
                 <div wire:loading wire:target='addUser' class="spinner-border" role="status">
                     <span class="visually-hidden">Loading...</span>
-                  </div>
+                </div>
             </div>
         </form>
     </div>
 
     <div class="col md-6">
-        <div class="d-flex align-items-center gap-3">
-            <button wire:click.throttle.3000ms="$refresh" class="btn btn-success mb-2">Refresh</button>
-            <div wire:loading class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
-        </div>
         <ul>
             @forelse ($users as $user)
-                <li wire:transition wire:key="{{$user->id}}">{{ $user->name }} ({{ $user->email }}) |
-                    <a href="#" wire:click.prevent="deleteUser({{$user->id}})" wire:confirm="Are you sure?">Delete</a></li>
+                <li wire:key="{{ $user->id }}">{{ $user->name }} ({{ $user->email }}) |
+                    <a href="#" wire:click.prevent="delete({{ $user->id }})"
+                        wire:confirm="Are you sure?">Delete</a>
+                </li>
             @empty
                 <p>User list is empty...</p>
             @endforelse
