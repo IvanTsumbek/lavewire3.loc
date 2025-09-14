b026ef59c1ccde147cc5f461c2532937e3225ddd
IvanTsumbek <maincumbek@gmail.com>
2025-09-14 20:15:36 +0300

    Урок 14. Компонент DataTables. Поиск

--- app/Livewire/User/UserList.php
+++ app/Livewire/User/UserList.php
@@ -20,36 +20,63 @@ class UserList extends Component
 
     public array $limitList = [10, 25, 50, 100];
 
-    public function changeLimit($limit)
+    #[Url]
+    public string $search = '';
+
+    public function changeLimit()
     {
-        $limit = (int) $limit;
-        $this->limit = in_array($limit, $this->limitList) ? $limit : $this->limitList[0];
+        $this->limit = in_array($this->limit, $this->limitList) ? $this->limit : $this->limitList[0];
         $this->resetPage();
     }
 
     public function getLimit(): int
     {
-        return in_array($this->limit, $this->limitList) ? $this->limit: 
-        $this->limitList[0];
+        return in_array($this->limit, $this->limitList) ? $this->limit : $this->limitList[0];
     }
 
-        public function delete(int $id)
+    public function updating($property, $value)
+    {
+        if($property == 'search'){
+            $this->resetPage();
+        };
+    }
+
+    public function delete(int $id)
     {
         User::find($id)->delete();
     }
 
 
     #[On('user-created')]
-    public function updateUserList($user=null)
-    {}
+    public function updateUserList($user = null) {}
 
     public function render()
     {
+        // $users = User::query()
+        //     ->with('country')
+        //     ->when($this->search, function($query) {
+        //         $query->whereLike('name', '%' . $this->search . '%')
+        //         ->orWhereLike('email', '%' . $this->search . '%');
+        //     })
+        //     ->orderBy('id', 'desc')
+        //     ->paginate($this->getLimit());
+
+            $users = User::query()
+            ->select('users.id', 'users.name', 'users.email', 'countries.name as country_name')
+            ->join('countries', 'users.country_id', '=', 'countries.id')
+            ->when($this->search, function($query) {
+                $query->whereAny(['users.name', 'users.email', 'countries.name'], 
+                                  'like', '%' . $this->search . '%');
+
+                // $query->whereLike('users.name', '%' . $this->search . '%')
+                // ->orWhereLike('users.email', '%' . $this->search . '%')
+                // ->orWhereLike('countries.name', '%' . $this->search . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate($this->getLimit());
+
         return view('livewire.user.user-list', [
-            'users' => User::query()
-                            ->with('country')
-                            ->orderBy('id', 'desc')
-                            ->paginate($this->getLimit()),
+            'users' => $users,
         ]);
     }
 }
--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,9 +1,9 @@
 <div class="col-12">
 
     <div class="d-flex justify-content-between mb-2" id="users-list">
-        
+     
         <div>
-            <select class="form-select" wire:change="changeLimit($event.target.value)">
+            <select class="form-select" wire:model="limit" wire:change="changeLimit">
                 @foreach ($limitList as $k => $v)
                 <option 
                     @if($v == $limit) selected @endif 
@@ -28,7 +28,8 @@
         </div>
 
         <div>
-            <input type="text" class="form-control" id="search" placeholder="Search...">
+            <input type="text" class="form-control" id="search" placeholder="Search..."
+            wire:model.live.debounce.300ms="search">
         </div>
 
     </div>
@@ -60,7 +61,7 @@
                         <td>{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
-                        <td>{{ $user->country->name }}</td>
+                        <td>{{ $user->country_name }}</td>
                         <td><button wire:click="delete({{ $user->id }})" wire:confirm="Are you sure?"
                                 class="btn btn-danger">Delete</a>
                         </td>
